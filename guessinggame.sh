5563eb621f1da533ce343da04a9e4843e726d491
kylehiroyasu <kyle.hiroyasu@utah.edu>
2017-09-25 21:52:25 +0200

    guessing game finished

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,10 +3,10 @@
 
 #create simple function to determine if the guess is correct
 function highorlow {
-    if [[ $1 -eq $2 ]]
+    if [[ $1 -eq $2 ]];
     then
 	echo "congrats"
-    elif [[ $1 -lt $2 ]]
+    elif [[ $1 -lt $2 ]];
     then
 	echo "$1 is too low"
     else
@@ -20,10 +20,16 @@ nfiles=$(ls | wc -l)
 echo $nfiles
 
 #Create local variables for the guessing game
+guess=-1
 
 # While the users guess is incorrect keep looping through game
-
-#check if higher or lower
-
+while [[ ! guess -eq nfiles ]];
+do
+    #get user input
+    echo "Please guess the number of files: "
+    read guess
+    #check if higher or lower
+    highorlow $guess $nfiles
+done
 #if we've gotten out of while loop the user must have guessed right
 
